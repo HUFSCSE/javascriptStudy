414719ac2ee02d1589e7911b3d5ddc595b9b3599
yiheechan <lhc4002@gmail.com>
2017-05-21 09:59:20 +0900

    5주차 실습자료

---
+++ 실습/이희찬/chap5/chap5_3.js
@@ -0,0 +1,13 @@
+var value = "value1";
+
+function printFunc(func) {
+    var value = "value2";
+	
+	function printValue() {
+		return value;
+	}
+	
+    console.log(printValue());
+}
+
+printFunc();
